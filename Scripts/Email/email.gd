63717b166838eb4e1ccd04415d9319a6efa3a0b8
ByDesign-213 <13davis.brandon@gmail.com>
2026-07-22 13:14:55 -0600

    Basic Email Scene - init
    Created basic email scene with simple script

---
+++ Scripts/Email/email.gd
@@ -0,0 +1,34 @@
+extends Node2D
+class_name Email
+
+@export_group("Scripts")
+
+@export_group("Data")
+@export_enum("Normal") var type : String
+
+var is_hovering : bool = false
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func delete_email(): # delete email after doing little animation
+	# add in code animation
+	queue_free()
+
+
+
+# special interaction stuff
+func _on_read_pressed():
+	delete_email()
+
+func _on_yes_pressed():
+	delete_email()
+
+func _on_no_pressed():
+	delete_email()
+
+func _on_delete_pressed():
+	delete_email()
