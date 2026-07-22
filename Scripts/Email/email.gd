8acebc12da2684af32250b8a8d492a567043554a
ByDesign-213 <13davis.brandon@gmail.com>
2026-07-22 14:29:10 -0600

    Added different simple types of Emails
    There are now 4 types. some you just read, some you accept, others you decline, and spam.

--- Scripts/Email/email.gd
+++ Scripts/Email/email.gd
@@ -4,29 +4,49 @@ class_name Email
 @export_group("Scripts")
 
 @export_group("Data")
-@export_enum("Normal") var type : String
+@export_enum("Normal", "Accept", "Decline", "Spam") var type : String
 
 func _ready():
-	pass
+	var random : float = randf_range(1, 100)
+	if random < 70:
+		type = "Normal"
+		modulate = Color.WHITE
+	elif random < 80:
+		type = "Accept"
+		modulate = Color.LIGHT_GREEN
+	elif random < 90:
+		type = "Decline"
+		modulate = Color.LIGHT_CORAL
+	elif random <= 100:
+		type = "Spam"
+		modulate = Color.LIGHT_SALMON
 
 func _process(delta):
 	pass
 
-func delete_email(): # delete email after doing little animation
-	# add in code animation
-	queue_free()
+func delete_email(success : bool): # delete email after doing little animation
+	if success:
+		# add in code animation
+		# add score
+		
+		queue_free()
+	else:
+		# add in code animation
+		# add penalty
+		
+		queue_free()
 
 
 
 # special interaction stuff
 func _on_read_pressed():
-	delete_email()
+	delete_email(type == "Normal")
 
 func _on_yes_pressed():
-	delete_email()
+	delete_email(type == "Accept")
 
 func _on_no_pressed():
-	delete_email()
+	delete_email(type == "Decline")
 
 func _on_delete_pressed():
-	delete_email()
+	delete_email(type == "Spam")
