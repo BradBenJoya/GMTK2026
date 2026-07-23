154647441abdd825e9e993fd4ab87931355af588
BradBenJoya <bradbenjoya6@gmail.com>
2026-07-23 16:40:40 +0300

    Added export to the names of the upgrades

--- Scripts/Cards/card_base.gd
+++ Scripts/Cards/card_base.gd
@@ -1,7 +1,22 @@
+# start Psuedo Pakman
 extends Button
+class_name BaseUpgrade
 
-func pressed() -> void:
-	print(self.text)
+@export_group("Names")
+
+enum UpgradeType {
+	FASTER_UPLOAD,
+	BETTER_ANTIVIRUS,
+	SPAM_ORGNIZER
+}
+
+var upgrade_type: UpgradeType
 
 func _ready() -> void:
 	connect("pressed", pressed)
+	# Get the values inside the enum as an array and then pick a random type to assign
+	upgrade_type = UpgradeType.values().pick_random()
+
+func pressed() -> void:
+	print(text)
+# end Psuedo Pakman
