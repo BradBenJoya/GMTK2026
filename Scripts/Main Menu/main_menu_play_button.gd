9cbb60d2846187dd1d64970a6fbf8490963e0358
BradBenJoya <bradbenjoya6@gmail.com>
2026-07-23 09:57:38 +0300

    Added some comments for clarity

--- Scripts/Main Menu/main_menu_play_button.gd
+++ Scripts/Main Menu/main_menu_play_button.gd
@@ -2,24 +2,24 @@
 extends MeshInstance2D
 class_name PlayButton
 
-signal play_pressed
+signal play_pressed ## Detects if the "Play" button has been pressed
 
 @export var temp_play_button_mouse_detection: StaticBody2D
 var has_mouse_entered: bool = false
 
 func _ready() -> void:
+	# Connect the StaticBody2D signals for mouse entered/exited
 	temp_play_button_mouse_detection.mouse_entered.connect(_on_mouse_entered)
 	temp_play_button_mouse_detection.mouse_exited.connect(_on_mouse_exited)
 
 func _on_mouse_entered() -> void:
-	print("mouse enterd")
 	has_mouse_entered = true
 
 func _on_mouse_exited() -> void:
-	print("mouse exited")
 	has_mouse_entered = false
 
 func _input(event: InputEvent) -> void:
+	# Emit the signal if the mouse was clicked on the "Play" button
 	if has_mouse_entered and event.is_action_pressed("Click"):
 		play_pressed.emit()
 # end Psuedo Pakman
