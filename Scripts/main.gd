2041ae855bb3c3d6a0da02ddd038cf3270d63238
ByDesign-213 <13davis.brandon@gmail.com>
2026-07-23 19:46:08 -0600

    added fonts, fixed email stuff, all choices on emails, updated images

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,12 +1,28 @@
 # start ByDesign
 extends Node2D
 
+var clock : float = 0.0 # + 9 to it and go to 7 to simulate 9 to 5
 var total_emails : int = 0
 
+@export var counter : Label
+@export var time : Label
+
+@export var day_duration : float = 300.0
+
 func _ready():
-	pass
+	var tween_time = create_tween().tween_property(self, "clock", 8.0, 300.0)
+	await tween_time.finished
+	print("end game")
 
 func _process(delta):
-	$TempScore.text = "Emails \n" + str(total_emails)
+	counter.text = "Inbox " + str(total_emails)
+	
+	# clock stuff! :D
+	if round(clock + 9) > 12:
+		time.text = str(int(fmod(round(clock + 9), 12))) + " PM"
+	elif fmod(round(clock + 9), 12) == 0:
+		time.text = "12 PM"
+	else:
+		time.text = str(int(fmod(round(clock + 9), 12))) + " AM"
 
 # end ByDesign
