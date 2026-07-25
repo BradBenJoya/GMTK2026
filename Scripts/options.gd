3fb821ef9f540a679de26c4ea07b0c195fbe920e
psiblackwood <153527824+psiblackwood@users.noreply.github.com>
2026-07-25 20:19:59 +0100

    improved settings value updates

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -3,11 +3,16 @@ extends PanelContainer
 
 func _ready() -> void:
 	%MasterSlider.value = Global._master * 100
-	%MasterValue.text = str(%MusicSlider.value)
+	%MasterValue.text = str(int(%MasterSlider.value)) + "%"
 	%MusicSlider.value = Global._music * 100
-	%MusicValue.text = str(%MusicSlider.value)
+	%MusicValue.text = str(int(%MusicSlider.value)) + "%"
 	%SFXSlider.value = Global._sfx * 100
-	%SFXValue.text = str(%SFXSlider.value)
+	%SFXValue.text = str(int(%SFXSlider.value)) + "%"
+	
+	%MouseXSlider.value = Global._mouse_sensitivity.x
+	%MouseXValue.text = "%.2f" % %MouseXSlider.value
+	%MouseYSlider.value = Global._mouse_sensitivity.y
+	%MouseYValue.text = "%.2f" % %MouseYSlider.value
 
 func _process(delta: float) -> void:
 	pass
@@ -15,19 +20,19 @@ func _process(delta: float) -> void:
 
 func _on_master_slider_drag_ended(value_changed: bool) -> void:
 	Global._master = %MasterSlider.value / 100
-	%MasterValue.text = str(Global._master * 100)
+	%MasterValue.text = str(int(Global._master * 100)) + "%"
 	AudioServer.set_bus_volume_db(Global.master_bus, linear_to_db(Global._master))
 
 
 func _on_music_slider_drag_ended(value_changed: bool) -> void:
 	Global._music = %MusicSlider.value / 100
-	%MusicValue.text = str(Global._music * 100)
+	%MusicValue.text = str(int(Global._music * 100)) + "%"
 	AudioServer.set_bus_volume_db(Global.music_bus, linear_to_db(Global._music))
 
 
 func _on_sfx_slider_drag_ended(value_changed: bool) -> void:
 	Global._sfx = %SFXSlider.value / 100
-	%SFXValue.text = str(Global._sfx * 100)
+	%SFXValue.text = str(int(Global._sfx * 100)) + "%"
 	AudioServer.set_bus_volume_db(Global.sfx_bus, linear_to_db(Global._sfx))
 
 
@@ -40,6 +45,21 @@ func _on_mouse_y_slider_drag_ended(value_changed: bool) -> void:
 	Global._mouse_sensitivity.y = %MouseYSlider.value
 	%MouseYValue.text = str(%MouseYSlider.value)
 	print(Global._mouse_sensitivity)
+
+func _on_master_slider_value_changed(value: float) -> void:
+	%MasterValue.text = str(int(value)) + "%"
+
+func _on_music_slider_value_changed(value: float) -> void:
+	%MusicValue.text = str(int(value)) + "%"
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	%SFXValue.text = str(int(value)) + "%"
+
+func _on_mouse_x_slider_value_changed(value: float) -> void:
+	%MouseXValue.text = "%.2f" % value
+
+func _on_mouse_y_slider_value_changed(value: float) -> void:
+	%MouseYValue.text = "%.2f" % value
 
 func _on_option_button_item_selected(index: int) -> void:
 	Global._window_mode = index
